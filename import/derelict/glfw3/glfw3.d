4e62822d47f709421cdda2dce7c5c7075de2980d
Mike Parker <aldacron@gmail.com>
2012-06-17 18:57:09 +0900

    Added /usr/lib/local/libglfw.so to the default search path in DerelictGLFW3.

--- import/derelict/glfw3/glfw3.d
+++ import/derelict/glfw3/glfw3.d
@@ -43,7 +43,7 @@ private
     else static if(Derelict_OS_Mac)
         enum libNames = "libglfw.dylib";
     else static if(Derelict_OS_Posix)
-        enum libNames = "libglfw.so";
+        enum libNames = "libglfw.so,/usr/local/lib/libglfw.so";
     else
         static assert(0, "Need to implement GLFW libNames for this operating system.");
 }
